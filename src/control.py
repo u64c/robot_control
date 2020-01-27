922ba6527a8a9be9321208f7ac81a07177c83055
u64c <mail@christophscharnagl.de>
2020-01-27 23:31:26 +0100

    add Twist in callback

--- src/control.py
+++ src/control.py
@@ -2,6 +2,7 @@
 import rospy
 from std_msgs.msg import String
 from std_msgs.msg import Int32
+from geometry_msgs.msg import Twist
 import RPi.GPIO as GPIO
 
 # import the library
@@ -14,9 +15,15 @@ rightDirection= 26       	# Direction -> GPIO Pin
 rightStep = 19      		# Step -> GPIO Pin
 
 # define GPIO pins left wheel
-leftMs13 = (2, 3, 4) 	# Microstep Resolution MS1-MS3 -> GPIO Pin
+leftMs13 = (2, 3, 4) 		# Microstep Resolution MS1-MS3 -> GPIO Pin
 leftDirection= 27       	# Direction -> GPIO Pin
-leftStep = 17      			# Step -> GPIO Pin
+leftStep = 17      		# Step -> GPIO Pin
+
+activateSteppers = 23
+activateLED = 22
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(activateSteppers,GPIO.OUT)
+GPIO.setup(activateLED,GPIO.OUT)
 
 # Declare a instance of class pass GPIO pins numbers and the motor type of the right and left wheel
 RightWheel = RpiMotorLib.A4988Nema(rightDirection, rightStep, rightMs13, "DRV8825") 
@@ -29,39 +36,56 @@ def callback(data):
 def listener():
 	rospy.init_node('listener', anonymous=True)
 	
-	rospy.Subscriber("left", Int32, runRightWheel)
-	rospy.Subscriber("right", Int32, runLeftWheel)
+	rospy.Subscriber("robCommandLeft", Twist, runRightWheel)
+	rospy.Subscriber("robCommandRight", Twist, runLeftWheel)
 	
 	rospy.spin()
 
 
-def runLeftWheel(stepNumber, dir=True, stepDelay=0.0005):
+def runLeftWheel(data):
 # sends signals to the left motor driver...
 	# call the function, pass the arguments
 	#RightWheel.motor_go(dir, "1/16" , stepNumber, stepDelay, False, .05)	 	# direction: False CC True CCC, number of steps, step delay, verbose output, init delay
 	#steps = int(stepNumber)
 	#print("Type of StepNumber: ", type(steps))
 	#stepNumber = int(stepNumber)
-	print("Im here")
-	LeftWheel.motor_go(dir, "1/16" , 20000, stepDelay, False, .05)                     # direction: False CC True CCC, number of steps, step delay, verbose output, init delay
+	print("left command: ", type(data.linear.x))
+	stepNumber = int(data.angular.z)
+	if (int(data.linear.y) == 1):
+	    dir = True
+	else:
+	    dir = False
+	steps = data.linear.x
+	print("stepsLeft: ", data.linear,x)
+	LeftWheel.motor_go(dir, "1/16" , stepNumber, steps, False, .05)                     # direction: False CC True CCC, number of steps, step delay, verbose output, init delay
 
 
-def runRightWheel(stepNumber, dir=False, stepDelay=0.0005):
+def runRightWheel(data):
 #sends signals to the right motor driver...
 	# call the function, pass the arguments
 	#steps = int(stepNumber)
 	#print("Type of StepNumber: ", type(steps))
-	#stepNumber = int(stepNumber)
-	print("Im here")
-	RightWheel.motor_go(dir, "1/16" , 20000, stepDelay, False, .05)			# direction: False CC True CCC, number of steps, step delay, verbose output, init delay
+	stepNumber = int(data.angular.z)
+	print("right command: ", type(data.linear.x))
+	if (int(data.linear.y) == 1):
+	    dir = True
+	else:
+	    dir = False
+	steps = data.linear.x
+	print("stepsRight: ", steps)
+	RightWheel.motor_go(dir, "1/16" , stepNumber, steps, False, .05)			# direction: False CC True CCC, number of steps, step delay, verbose output, init delay
 
 def main():
 	#runWheel(False,2000,0.0005);
-	#runLeftWheel(True,2000,0.0005);
-	listener()
+	#runLeftWheel(True,2000,0.0005
+
+	listener() 
+
     
 # ===== Programmstart =====       
 if __name__ == "__main__":
+    GPIO.output(activateSteppers, GPIO.HIGH)
+    GPIO.output(activateLED, GPIO.HIGH) 
     main()
 
 
